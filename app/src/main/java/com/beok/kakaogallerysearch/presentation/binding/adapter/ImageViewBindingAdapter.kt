962096534a4208070633d9743d55f7533e33cfc1
강현석 <kekemusa37@gmail.com>
2022-03-04 20:45:16 +0900

    :sparkles: [적용] 이미지 로드중 이미지

--- app/src/main/java/com/beok/kakaogallerysearch/presentation/binding/adapter/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/beok/kakaogallerysearch/presentation/binding/adapter/ImageViewBindingAdapter.kt
@@ -2,11 +2,13 @@ package com.beok.kakaogallerysearch.presentation.binding.adapter
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.beok.kakaogallerysearch.R
 import com.bumptech.glide.Glide
 
 @BindingAdapter("bind_glide_imageUrl")
 fun setGlideImageUrl(view: ImageView, url: String) {
     Glide.with(view)
         .load(url)
+        .placeholder(R.mipmap.ic_launcher_round)
         .into(view)
 }
